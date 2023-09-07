a278aaed5563bbad0da8c3113454c23cc1f1d900
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-09-07 22:46:04 +0600

    service card added

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import ServicesCard from "./ServicesCard";
+
+const Services = () => {
+  const [services, steServices] = useState([]);
+  fetch("services.json")
+    .then((res) => res.json())
+    .then((data) => steServices(data));
+  return (
+    <div className="mt-4">
+      <div className="text-center">
+        <h2 className="text-2xl font-bold text-orange-600">Service</h2>
+        <h3 className="text-5xl">Our Service Area</h3>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised <br />
+          words which don't look even slightly believable.{" "}
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {services.map((service) => (
+            <ServicesCard key={service._id} service={service}></ServicesCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
