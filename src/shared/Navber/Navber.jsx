44a8b9d6a52073531240dec48014b8b0438ce66a
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-09-07 17:42:14 +0600

    navber footer and bannner added

---
+++ src/shared/Navber/Navber.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/icons/check.svg";
+
+const Navber = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 h-28 mb-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <li
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3  p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </li>
+        </div>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <li className="menu menu-horizontal px-1">{navItems}</li>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-outline btn-warning">Appoinment</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
